cf22b90eaa54502e06e77e66a67e80f850c7d6d5
unknown <andrey09767@gmail.com>
2021-09-22 20:30:46 +0300

    сложение чисел

---
+++ 1 week/1 task/1 task.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int firNum = 0, secNum = 0;
+    cin >> firNum >> secNum;
+    cout << firNum + secNum;
+    system("PAUSE");
+    return 0;
+}
