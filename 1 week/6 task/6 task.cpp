d40e909bfba4766658afd15db09f3581f05aa9e1
unknown <andrey09767@gmail.com>
2021-09-23 21:10:08 +0300

    чётные числа

---
+++ 1 week/6 task/6 task.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    int a = 0, b = 0;
+    std::cin >> a >> b;
+    for(int i = a; i <= b; i++){
+        if(i % 2 == 0)
+            std::cout << i << " ";
+    }
+    return 0;
+}
