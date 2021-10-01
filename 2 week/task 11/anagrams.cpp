1ce84be2f2660b346bd42eecd175ed8dab8fa0e8
unknown <andrey09767@gmail.com>
2021-10-01 12:53:05 +0300

    анаграммы

---
+++ 2 week/task 11/anagrams.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <map>
+bool isEqual(std::map<char, int> firstW, std::map<char, int> secondW)
+{
+    if (firstW.size() != secondW.size()) {
+        return false;
+    }
+    else {
+        if (firstW == secondW)
+            return true;
+        else
+            return false;
+    }
+}
+
+void isAnagram(std::string first, std::string second)
+{
+    std::map<char, int> firstW;
+    std::map<char, int> secondW;
+   
+        for (auto i : first)
+            ++firstW[i];
+
+        for (auto i : second)
+            ++secondW[i];
+
+        if (isEqual(firstW, secondW)) {
+            firstW.clear();
+            secondW.clear();
+            std::cout << "YES" << std::endl;
+        } else {
+            firstW.clear();
+            secondW.clear();
+            std::cout << "NO" << std::endl;
+        }
+           
+
+}
+int main()
+{
+    int n = 0;
+    std::string str;
+    std::string str2;
+    std::cin >> n;
+    for (int i = 0; i < n; ++i) {
+        std::cin >> str;
+        std::cin >> str2;
+        isAnagram(str, str2);  
+    }
+
+    return 0;
+}
